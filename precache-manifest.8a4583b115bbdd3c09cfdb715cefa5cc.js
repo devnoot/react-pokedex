46bbcbd1ca39c60dcebd36431254d8244b81b1f1
devnoot <devnoot@gmail.com>
2019-10-12 19:38:17 -0400

    Updates

---
+++ precache-manifest.8a4583b115bbdd3c09cfdb715cefa5cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17543e97d42475979761f4a8cd9d35a4",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "2f7b8041cf894e37e44d",
+    "url": "/react-pokedex/static/css/main.d04d5b0e.chunk.css"
+  },
+  {
+    "revision": "d55bf8bde6993675a811",
+    "url": "/react-pokedex/static/js/2.9dcafd15.chunk.js"
+  },
+  {
+    "revision": "2f7b8041cf894e37e44d",
+    "url": "/react-pokedex/static/js/main.185984fb.chunk.js"
+  },
+  {
+    "revision": "f41b2b5d430533d03d3e",
+    "url": "/react-pokedex/static/js/runtime-main.546470a4.js"
+  }
+]);
