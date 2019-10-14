754b933fe5f8e4675fd67f537a4c577c98f4b585
devnoot <devnoot@gmail.com>
2019-10-13 23:45:17 -0400

    Adds react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import Selector from "./components/Selector";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Badge from "react-bootstrap/Badge";
 
 import "./App.css";
 
@@ -12,7 +17,7 @@ class App extends Component {
     nextURL: "",
     prevURL: "",
     offset: 0,
-    limit: 15
+    limit: 10
   };
 
   componentDidMount() {
@@ -75,85 +80,102 @@ class App extends Component {
       isLoading,
       offset,
       limit,
-      nextURL,
       prevURL
     } = this.state;
 
+    /* 
+    The pokedex can have one of two views, the first view is a selection 
+    on the right side of the screen, with a portrait of the pokemon on
+    the left side. The second view is invoked when the user "clicks" on 
+    the pokemon from the scroll list. This view is a horizontal split
+    wherein, on the top is an image of the pokemon, along with some stats.
+    The bottom portion is the description of the pokemon. This view is 
+    very similar to the Gen 1 pokedex in the Pokemon games.
+    */
+
     return (
       <div className="App">
-        <main>
-          <div className="Container">
-            {/* First, if there's an error, display it */}
-            {error && <p>{error}</p>}
+        <main className="mt-4">
+          {/* First, if there's an error, display it */}
+          {error && <p>{error}</p>}
 
-            {/* 
-            The pokedex can have one of two views, the first view is a selection 
-            on the right side of the screen, with a portrait of the pokemon on
-            the left side. The second view is invoked when the user "clicks" on 
-            the pokemon from the scroll list. This view is a horizontal split
-            wherein, on the top is an image of the pokemon, along with some stats.
-            The bottom portion is the description of the pokemon. This view is 
-            very similar to the Gen 1 pokedex in the Pokemon games.
-            */}
-
-            {/* When a pokemon is selected, display the details here */}
-            <div className="Details">
-              {isLoading && <div className="loader"></div>}
-              {selected && !isLoading && (
-                <div style={{ padding: "1em" }}>
-                  <h3>
-                    #{selected.id}{" "}
-                    <span className="capitalize">{selected.name}</span>
-                  </h3>
-                  <p>
-                    {+selected.height * 10} cm / {+selected.weight / 10} kg
-                  </p>
-                  <img
-                    src={selected.sprites.front_default}
-                    alt={`${selected.name} normal form`}
+          <Container>
+            <Row>
+              <Col>
+                {/* When a pokemon is selected, display the details here */}
+                {isLoading && <div className="loader"></div>}
+                {selected && !isLoading && (
+                  <div style={{ padding: "1em" }}>
+                    <h3>
+                      <Badge variant="primary">#{selected.id}</Badge>
+                      <span className="ml-2 text-capitalize">
+                        {selected.name}
+                      </span>
+                    </h3>
+                    <p>
+                      {+selected.height * 10} cm / {+selected.weight / 10} kg
+                    </p>
+                    <img
+                      src={selected.sprites.front_default}
+                      alt={`${selected.name} normal form`}
+                    />
+                    <img
+                      src={selected.sprites.front_shiny}
+                      alt={`${selected.name} shiny form`}
+                    />
+                  </div>
+                )}
+              </Col>
+              <Col>
+                <p className="text-muted text-uppercase">
+                  Displaying {+offset} - {+offset + +limit}
+                </p>
+                
+                {/*
+                For each pokemon that should be shown (there should be a max
+                limit) display them in a list 
+                */}
+                {selectionList && (
+                  <Selector
+                    data={selectionList}
+                    onSelectPokemon={pokemon => this.onSelect(pokemon)}
                   />
-                  <img
-                    src={selected.sprites.front_shiny}
-                    alt={`${selected.name} shiny form`}
-                  />
-                </div>
-              )}
-            </div>
+                )}
 
-            {/*
-            For each pokemon that should be shown (there should be a max
-            limit) display them in a list 
-            */}
-
-            <div className="Selector">
-              {selectionList && (
-                <Selector
-                  data={selectionList}
-                  onSelectPokemon={pokemon => this.onSelect(pokemon)}
-                />
-              )}
-
-              {/*
+                {/*
                 There should be a scroll up and scroll down action available, doing so should
                 grab the next "page" of Pokemon from the API
               */}
-              <div className="Selector-Actions">
-                <button onClick={() => this.prev()}>prev</button>
-                <button onClick={() => this.next()}>next</button>
-              </div>
-            </div>
-
-            <footer className="Footer">
-              <p className="text-center">
-                Displaying {+offset} - {+offset + +limit}
-              </p>
-              <p className="text-center">
-                Built by <a href="https://twitter.com/devnoot">devnoot</a> using{" "}
-                <a href="https://pokespi.co">PokeAPI</a>
-              </p>
-            </footer>
-          </div>
+                <div className="mt-3">
+                  <Button
+                    className="mr-2"
+                    onClick={() => this.prev()}
+                    variant="text"
+                    disabled={!prevURL}
+                  >
+                    Back
+                  </Button>
+                  <Button onClick={() => this.next()} variant="text">
+                    Next
+                  </Button>
+                </div>
+              </Col>
+            </Row>
+          </Container>
         </main>
+
+        <footer className="mt-5">
+          <Container>
+            <Row>
+              <Col>
+                <p className="text-center text-muted">
+                  Built by <a href="https://twitter.com/devnoot">devnoot</a>{" "}
+                  using <a href="https://pokespi.co">PokeAPI</a>
+                </p>
+              </Col>
+            </Row>
+          </Container>
+        </footer>
       </div>
     );
   }
--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,26 +1,27 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import ListGroup from "react-bootstrap/Listgroup";
 
 class Selector extends Component {
-
-  state = {}
+  state = {};
 
   onSelect(pokemon) {
     const { onSelectPokemon } = this.props;
-    this.setState({ selected: pokemon }); 
-    onSelectPokemon(pokemon); 
+    this.setState({ selected: pokemon });
+    onSelectPokemon(pokemon);
   }
 
   render() {
     const { data } = this.props;
 
     return (
-      <div style={{ padding: "0.5em 1em" }}>
-        {data.map((pkmn, i) => <div className="Selector-Item" key={i}>
-          <a href="#" onClick={() => this.onSelect(pkmn)}><p className="capitalize">{pkmn.name}</p></a>
-          </div>
-        )} 
-      </div> 
+      <ListGroup>
+        {data.map((pkmn, i) => (
+          <ListGroup.Item key={i} onClick={() => this.onSelect(pkmn)} action className="text-capitalize" active={pkmn === this.state.selected}>
+            {pkmn.name}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
     );
   }
 }
@@ -29,7 +30,7 @@ Selector.propTypes = {
   data: PropTypes.arrayOf(
     PropTypes.shape({
       name: PropTypes.string.isRequired,
-      url: PropTypes.string.isRequired 
+      url: PropTypes.string.isRequired
     })
   ).isRequired
 };
